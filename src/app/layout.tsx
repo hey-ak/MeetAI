6cca2e660a1a29d3edbf8c8aee3bd6b7c74535df
hey-ak <akshay0127.be21@chitkara.edu.in>
2025-08-22 20:52:51 +0530

    add agents filters

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/client";
+import {NuqsAdapter} from "nuqs/adapters/next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
@@ -22,16 +23,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <NuqsAdapter>
+
     <TRPCReactProvider>
  <html lang="en">
       <body
         className={`${inter.className} antialiased`}
-      >
+        >
         <Toaster/>
         {children}
       </body>
     </html>
     </TRPCReactProvider>
+        </NuqsAdapter>
    
   );
 }
